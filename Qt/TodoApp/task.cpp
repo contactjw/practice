39b9ec25eb3237248e550ee86b903cae9d8eb418
John West <contactjohnwest@gmail.com>
2019-07-06 21:12:47 -0700

    First commit

---
+++ Qt/TodoApp/task.cpp
@@ -0,0 +1,45 @@
+#include "Task.h"
+#include "ui_task.h"
+
+#include <QInputDialog>
+
+Task::Task(const QString& name, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Task)
+{
+    ui->setupUi(this);
+    setName(name);
+    connect(ui->editButton, &QPushButton::clicked, this, &Task::rename);
+}
+
+Task::~Task()
+{
+    delete ui;
+}
+
+void Task::setName(const QString& name)
+{
+    ui->checkbox->setText(name);
+}
+
+QString Task::name() const
+{
+    return ui->checkbox->text();
+}
+
+bool Task::isCompleted() const
+{
+    return ui->checkbox->isChecked();
+}
+
+void Task::rename()
+{
+    bool ok;
+    QString value = QInputDialog::getText(this, tr("Edit task"),
+                                          tr("Task name"),
+                                          QLineEdit::Normal,
+                                          this->name(), &ok);
+}
+
+
+
