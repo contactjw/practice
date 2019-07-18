76ecc8c70f6d5a6f3cd5c4c391763991521c5afa
John West <contactjohnwest@gmail.com>
2019-07-17 21:04:43 -0700

    Added cargoship class that overrides ship's virtual print function

---
+++ C++/ships-virtual/ships-virtual/CargoShip.h
@@ -0,0 +1,40 @@
+#ifndef CargoShip_h
+#define CargoShip_h
+#include <iostream>
+#include "Ship.h"
+using namespace std;
+
+class CargoShip : public Ship
+{
+protected:
+    int cargoWeight;
+public:
+    CargoShip()
+    {
+        cargoWeight = 0;
+    }
+    
+    CargoShip(int weight, string name, string year) : Ship(name, year)
+    {
+        cargoWeight = weight;
+    }
+    
+    void setWeight(int weight)
+    {
+        cargoWeight = weight;
+    }
+    
+    int getWeight()
+    {
+        return cargoWeight;
+    }
+    
+    void print()
+    {
+        cout << "Name: " << Ship::getName() << endl;
+        cout << "Weight: " << getWeight() << " tons" << endl;
+    }
+    
+};
+
+#endif /* CargoShip_h */
--- C++/ships-virtual/ships-virtual/main.cpp
+++ C++/ships-virtual/ships-virtual/main.cpp
@@ -2,12 +2,12 @@
 #include <string>
 #include <iomanip>
 #include "CruiseShip.h"
+#include "CargoShip.h"
 using namespace std;
 
 int main()
 {
     Ship *boat = new Ship("Titanic", "1775");
-    
     boat->print();
     
     CruiseShip *ship = new CruiseShip();
@@ -16,8 +16,17 @@ int main()
     
     ship->setName("Lusitania");
     ship->setPassengers(15000);
-    
     ship->print();
+    
+    cout << endl;
+    
+    CargoShip *cargo = new CargoShip();
+    
+    cargo->setName("Transporter");
+    cargo->setWeight(140);
+    cargo->print();
+
+    
     
     return 0;
 }
