0dadb2cbf755264fbf0cfb1c94d88fcdc0fda4fc
John West <contactjohnwest@gmail.com>
2019-07-20 15:29:16 -0700

    Added two derived shape classes, circle and rectangle

---
+++ C++/pure-abstract-base-class/pure-abstract-base-class/Rectangle.h
@@ -0,0 +1,36 @@
+#ifndef Rectangle_h
+#define Rectangle_h
+#include <iostream>
+#include "BasicShape.h"
+using namespace std;
+
+class Rectangle : public BasicShape
+{
+private:
+    long int width;
+    long int length;
+public:
+    Rectangle(long int w, long int len)
+    {
+        width = w;
+        length = len;
+    }
+    
+    long int getWidth()
+    {
+        return width;
+    }
+    
+    long int getLength()
+    {
+        return length;
+    }
+    
+    void calcArea()
+    {
+        area = length * width;
+    }
+    
+};
+
+#endif /* Rectangle_h */
