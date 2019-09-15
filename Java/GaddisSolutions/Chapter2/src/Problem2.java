19d0349f6635daf3e207997bc434d95635b4342d
John West <contactjohnwest@gmail.com>
2019-09-14 20:53:51 -0700

    Add problem 2 solution

---
+++ Java/GaddisSolutions/Chapter2/src/Problem2.java
@@ -0,0 +1,16 @@
+
+public class Problem2 {
+
+	public static void main(String[] args) {
+		String firstName = "John",
+			   middleName = "Joseph",
+			   lastName = "West";
+		char firstInitial = 'J',
+			 middleInitial = 'J',
+			 lastInitial = 'W';
+		
+		System.out.println("Name: " + firstName + " " + middleName + " " + lastName);
+		System.out.println("Initials: " + firstInitial + middleInitial + lastInitial);
+	}
+
+}
