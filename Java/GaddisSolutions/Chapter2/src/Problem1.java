bfb5535397dad0c00220531b16a92ff4850f35fa
John West <contactjohnwest@gmail.com>
2019-09-14 20:47:32 -0700

    Add problem 1 solution

---
+++ Java/GaddisSolutions/Chapter2/src/Problem1.java
@@ -0,0 +1,13 @@
+
+public class Problem1 {
+
+	public static void main(String[] args) {
+		String name = "John West";
+		int age = 24;
+		double annualPay = 120000;
+		
+		System.out.println("My name is " + name + ", my age is " + age + " and");
+		System.out.println("I hope to earn $" + annualPay + " per year.");
+	}
+
+}
